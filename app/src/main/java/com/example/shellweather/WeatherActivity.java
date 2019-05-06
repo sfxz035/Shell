bcb9ef5357d303b56b6e16815feac895d43e1301
dot <33475523+sfxz035@users.noreply.github.com>
2019-05-06 20:10:04 +0800

    修改图标和名称

--- app/src/main/java/com/example/shellweather/WeatherActivity.java
+++ app/src/main/java/com/example/shellweather/WeatherActivity.java
@@ -22,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.shellweather.gson.Forecast;
 import com.example.shellweather.gson.weather;
+import com.example.shellweather.service.AutoUpdateService;
 import com.example.shellweather.util.HttpUtil;
 import com.example.shellweather.util.Utility;
 
@@ -223,6 +224,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent =  new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 }
